7294814585c9c1a5c3ccb40a71bacca3afe8603c
Brandon Eng <bce25@cornell.edu>
2020-09-03 11:28:42 -0700

    Updates

---
+++ precache-manifest.ad923ad99a1af53576ee1096fd6ef025.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95f1065453d0d88c8110b71f8d178e6",
+    "url": "/ChartJSReactRentGraphs/index.html"
+  },
+  {
+    "revision": "f7a56e62fb0feb6b2152",
+    "url": "/ChartJSReactRentGraphs/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a287090f3c133be0fce4",
+    "url": "/ChartJSReactRentGraphs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f7a56e62fb0feb6b2152",
+    "url": "/ChartJSReactRentGraphs/static/js/2.93b70d80.chunk.js"
+  },
+  {
+    "revision": "381e1d6acf68c4f4a6a6161eae54c858",
+    "url": "/ChartJSReactRentGraphs/static/js/2.93b70d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a287090f3c133be0fce4",
+    "url": "/ChartJSReactRentGraphs/static/js/main.81471fda.chunk.js"
+  },
+  {
+    "revision": "a50612303992a3c0a31d",
+    "url": "/ChartJSReactRentGraphs/static/js/runtime-main.a1c8c56c.js"
+  },
+  {
+    "revision": "e2e30bc3ad039ecb8658f776edbe5eb3",
+    "url": "/ChartJSReactRentGraphs/static/media/apartment_rent.e2e30bc3.csv"
+  }
+]);
